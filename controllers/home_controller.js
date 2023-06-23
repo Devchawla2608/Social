3ffe18df125dbf6b71b7fd64af1171f7d2941758
Deepanshu Chawla <dev.chawla2608@gmail.com>
2023-06-23 20:28:34 +0530

    Post and Comments done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,45 @@
+const Post = require("../models/Post.js");
+// Single Population
+
+// module.exports.home = function (req, res) {
+//   // Populate user in every post to showing auther (Read Notes)
+//   Post.find({})
+//     .populate("user")
+//     .exec()
+//     .then((posts) => {
+//       console.log("Posts Found");
+//       return res.render("home", {
+//         title: "Home",
+//         posts: posts,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log("Error", err);
+//       return res.redirect("back");
+//     });
+// };
+
+// Nested Population
 module.exports.home = function (req, res) {
-  return res.render("home", {
-    title: "Home",
-  });
+  // Populate user in every post to showing auther (Read Notes)
+  Post.find({})
+    .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    })
+    .exec()
+    .then((posts) => {
+      console.log("Posts Found");
+      return res.render("home", {
+        title: "Home",
+        posts: posts,
+      });
+    })
+    .catch((err) => {
+      console.log("Error", err);
+      return res.redirect("back");
+    });
 };
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const homeController = require("../controllers/home_controller.js");
 
 router.get("/", homeController.home);
+router.use("/posts", require("./posts.js"));
 router.use("/users", require("./users.js"));
+router.use("/comments", require("./comments.js"));
 
 module.exports = router;
 
